cc2dd9a19e10b9f63a22792826dafe6fbf2e561b
Lisa Carvalho <i.m.lisaaa14@gmail.com>
2025-12-06 19:30:16 +0000

    MoneyGenAlly

--- Assets/Scripts/Behaviours/MoneyGenAlly.cs
+++ Assets/Scripts/Behaviours/MoneyGenAlly.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 namespace Behaviours
 {
@@ -6,10 +7,26 @@ namespace Behaviours
     {
         [SerializeField] private ViewManager viewManager;
         [SerializeField] private float moneyGenerated;
+        [SerializeField] private float moneyGenGap;
+        private bool _isActive;
 
         protected override void DoSomething()
         {
-            ;
+            if (_isActive)
+                StartCoroutine(GenMoney());
+        }
+
+        private IEnumerator GenMoney()
+        {
+            _isActive = false;
+            YieldInstruction wait = new WaitForSeconds(moneyGenGap);
+
+            while (true)
+            {
+                viewManager.EarnMoney(moneyGenerated);
+                
+                yield return wait;
+            }
         }
     }
 }
